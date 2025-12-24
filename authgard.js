736f786fd42f454c159e1186528f4bd46a82b997
Jugal pakhare <jugalpkhr@gmail.com>
2025-12-24 22:04:24 +0530

    Add files via upload

---
+++ authgard.js
@@ -0,0 +1,11 @@
+// authgard.js
+// authgard.js
+import { auth } from "./firebase.js";
+import { onAuthStateChanged }
+  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.replace("login.html");
+  }
+});
